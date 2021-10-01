7e65fce786d0c342bc5355b2136346bcfdb22131
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-01 20:42:59 +0530

    Yash Khandelwal 2 21bcon034.cpp

---
+++ Yash Khandelwal 21bcon034.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,b,c,d,e;
+cout<<"enter the value of income earned in the month a=";
+cin>>a;
+cout<<"enter the value of home expenditureof the month b=";
+cin>>b;
+cout<<"enter the value of liabilities of the month c=";
+cin>>c;
+cout<<"enter the valueof educational expenses of the month d=";
+cin>>d;
+e=a-(b+c+d)
+cout<<"the total savings of the month is e=";
+getch()
+  }
