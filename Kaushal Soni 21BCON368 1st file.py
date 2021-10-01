b6111f7c7dd7f4b97ebbd2225fe2cc614e901796
KaushalSony <91665722+KaushalSony@users.noreply.github.com>
2021-10-01 16:34:23 +0530

    Kaushal Soni 21BCON368 1st file

--- Kaushal Soni 21BCON368 1st file.py
+++ Kaushal Soni 21BCON368 1st file.py
@@ -1,4 +1,4 @@
 #program for printing reverse series for user given number
 a=int(input('Enter the number for which reverse series to be printed'))
-For i in range(a,0,-1):
-   print(i)
+For j in range(a,0,-1):
+   print(j)
