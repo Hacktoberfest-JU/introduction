ecc7cede64f2892300917d9710e09dfb0c9195eb
Dhruvgoyal21 <91668020+Dhruvgoyal21@users.noreply.github.com>
2021-10-01 15:44:39 +0530

    Create DhruvGoyal_21BCON424.cpp

---
+++ DhruvGoyal_21BCON424.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"ENTER NUM 1:\n";
+    cin>>num1;
+    cout<<"ENTER NUM 2:\n";
+    cin>>num2;
+    cout<<"the sum of the numbers is:\n"<<num1+num2;
+
+    return 0;
+}
