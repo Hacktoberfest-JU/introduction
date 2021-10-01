8d342746ee283fd052b836ff54ab62da6075f3cf
KaushalSony <91665722+KaushalSony@users.noreply.github.com>
2021-10-01 16:32:02 +0530

    Delete Kaushal Soni 21BCON368.py

--- Kaushal Soni 21BCON368.py
+++
@@ -1,4 +0,0 @@
-# This program is used for input a number and displaying its reverse series
-a=int(input('Enter a Number for which you want a reverse series'))
-for i in range (n,0):
-  print(i,end=' ')
