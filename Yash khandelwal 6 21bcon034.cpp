5f528306f9c2a91d7ca547f4c047a6e531704891
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-02 23:46:53 +0530

    Create Yash khandelwal 6 21bcon034.cpp

---
+++ Yash khandelwal 6 21bcon034.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,b;
+cout<<"enter the file name for placing pull request a=";
+cin>>a;
+cout<<"enter the code which should be entered in the file b=";
+cin>>b;
+cout<<"pull request successfully placed";
+getch();
+}
+  
