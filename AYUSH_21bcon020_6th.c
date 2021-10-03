f88812a9e1c85a42e21811e6aeb0ec40f7a95943
AYUSH SINGODIA <91623298+THEEBEAST@users.noreply.github.com>
2021-10-03 11:30:55 +0530

    Create AYUSH_21bcon020_6th.c

---
+++ AYUSH_21bcon020_6th.c
@@ -0,0 +1,14 @@
+//c progam to print star pattern //
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
