f0e5b3eca99fc508afa8e4a724df0c44604f2908
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-04 17:04:00 +0530

    Yash Khandelwal 7 21bcon034.cpp

---
+++ Yash 7 21bcon034.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+  {
+  clrscr() ;
+  int a,b;
+  cou<<"enter the upi id a=";
+  cin>>a;
+  cout<<"enter the payment amount b=";
+  cin>>b;
+  cout<<"wait for confirnation";
+  getch() ;
+  }
+© 2021 
