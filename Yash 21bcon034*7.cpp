bf8f08a5f28c1425aea4a9ee453a90965b6ca665
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-05 15:35:49 +0530

    Yash 21bcon034*7.cpp

---
+++ Yash 21bcon034*7.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+  {
+  clrscr();
+  int a,b;
+  cout<<"enter the students name a=\n";
+  cin>>a;
+  cout<<"enter book code b=\n";
+  cin>>b;
+  cout<<"success";
+  getch() ;
+  }
+  
+  
