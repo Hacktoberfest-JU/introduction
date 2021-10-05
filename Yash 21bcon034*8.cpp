77f038e72941f76dc4d8300c424abb02ee79710e
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-05 21:29:59 +0530

    Yash 21bcon034*8.cpp

---
+++ Yash 21bcon034*8.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+  {
+clrscr();
+int a, b;
+cout<<"enter the upi id of the beneficiary a=";
+cin>>a;
+cout<<"enter the amount to be transferred b=";
+cin>>b;
+cout<<"success";
+gtch();
+  }
+
+
+
+  
