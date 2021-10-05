342912bf13fbf083ebb050c66c6324a0f6b081dd
abhishek6420 <91943135+abhishek6420@users.noreply.github.com>
2021-10-05 14:02:06 +0530

    Create Abhishek_21bcon245_1st.py

---
+++ Abhishek_21bcon245_1st.py
@@ -0,0 +1,9 @@
+A = int( input("Please enter value for A: "))  
+B = int( input("Please enter value for B: "))  
+     
+C = A  
+A = B  
+B = C  
+   
+print ("The Value of A after swapping: ", A)  
+print ("The Value of B after swapping: ", B)
