9256b5219c0441b334aa378d830ca83924edaa8c
Tanish327 <91666582+Tanish327@users.noreply.github.com>
2021-10-06 12:33:36 +0530

    Tanish 21bcon012.cpp

---
+++ Tanish 21bcon012.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+  {
+  clrscr();
+  int a,b;
+  cout<<"enter the stock name a=";
+  cin>>a;
+  cout<<"enter the quantity b=";
+  cin>>b;
+  cout<<"success";
+  getch();
+  }
