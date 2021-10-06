2c6e881a12741f91844b00c52c7dd0ae602e37c4
Ayush SInghal <91676003+AyushSinghal9020@users.noreply.github.com>
2021-10-06 13:46:56 +0530

    Ayush_Singhal_21BCON102_04.cpp
    This code consists of a small mathematical operation (addition), in c++ language

---
+++ Ayush_SinghalL_21BCON102_04.cpp
@@ -0,0 +1,27 @@
+#include <iostream.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+clrscr();
+
+int a,b,sum;
+
+cout<<"Enter the first number : \n";
+
+cin>>a
+
+cout<<"Enter the second number : \n";
+
+cin>>b
+
+sum = a+b;
+
+cout<<"The sum of the two numbers is : \n";
+
+getch();
+
+}
