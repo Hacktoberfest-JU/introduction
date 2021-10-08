6a46d30b6b6a36ad2e21c94c744edf184f647d38
HelloShiv <91686009+HelloShiv@users.noreply.github.com>
2021-10-09 00:21:25 +0530

    UpdatedHotspotCreator.bat
    Updated hotspot creator new prompt added when the hotspot is created and stopped

---
+++ UpdatedHotspotCreator.bat
@@ -0,0 +1,36 @@
+@echo off
+
+@REM checking for privilege  Adminstrator usage .
+ net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Administrative permissions confirmed .
+    
+    ) else (
+        msg %USERNAME% Admininstrator Privilige Required
+        echo Failure: Current permissions inadequate PLEASE OPEN IT WITH ADMIN LEVEL privilege.
+        PAUSE
+        
+        exit
+    )
+
+echo PLEASE ENTER 8digit PASSWORD MUST
+SET /P hotspotKey=" Set hotspot password:"
+SET /P hotspotName="Set hotspot name:"
+
+netsh wlan set hostednetwork mode=allow ssid=%hotspotName% key=%hotspotKey%
+echo Initiating Hotspot
+Msg %USERNAME% Initiated Hotspot
+
+@REM Initiating Hotspot
+netsh wlan start hostednetwork
+
+cls  
+echo Please press FORGET NETWORK if you use the SAME NAME and DIFFERENT PASSWORD
+echo Hotspot Name:%hotspotName%
+echo Hotspot Key:%hotspotKey%
+
+@REM General variable defined to stop auto termination of command line could also use PAUSE
+SET /P WantToStopHotspot="Press ENTER to STOP hotspot: "
+
+netsh wlan stop hostednetwork
+Msg %USERNAME% Hotspot Stopped
