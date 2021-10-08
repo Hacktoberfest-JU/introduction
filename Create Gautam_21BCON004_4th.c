95f05531c8517e28e70523a039b69479fdc3652c
Gautam <91665681+rohillaGautam@users.noreply.github.com>
2021-10-08 19:47:27 +0530

    Create Create Gautam_21BCON004_4th.c

---
+++ Create Gautam_21BCON004_4th.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int length,breadth;
+    
+    printf("What is your length of your rectangle =\n");
+    scanf("%d",&length);
+    printf("What is your breadth of your rectangle =\n"); 
+    scanf("%d",&breadth);
+    
+    printf("Your area of rectangle is =%d", length*breadth);
+    return 0;
+}
