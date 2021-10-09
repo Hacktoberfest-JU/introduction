8dcd95924336680622b2e6857098928fdb05b0a7
kirtagya21bcon047 <91947719+kirtagya21bcon047@users.noreply.github.com>
2021-10-09 09:33:13 +0530

    kirtagya 21bcon047*2.cpp

---
+++ kirtagya 21bcon047*2.cpp
@@ -0,0 +1,10 @@
+#include<iosreaam.h>
+#include<conio.h>
+void main(){
+cout<<"the employee code of first 10 employees will be printed";
+// a is the employee code
+while(a<=10)
+{ cout<<a++;
+}
+getch();
+}
