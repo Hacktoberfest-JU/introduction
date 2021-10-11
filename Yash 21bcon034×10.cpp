7e7a9b198d54048edc2935215bf34c0d41610da4
YashKhandelwal2511 <90499117+YashKhandelwal2511@users.noreply.github.com>
2021-10-11 07:14:12 +0530

    Yash 21bcon034×10.cpp

---
+++ Yash 21bcon034×10.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+  {
+  int a=50,b;
+  if (a>b);
+  {
+    cout<<"price is cheap";
+    }
+  else
+    {
+    cout<<"price is costly";
+    }
+  getch();
+  }
+
+
+    
