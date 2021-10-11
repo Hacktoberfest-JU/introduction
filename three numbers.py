25cceb1a1221869b8278ac3c9709fe9b63d9b797
Deepanvita2021 <92332266+deepanvita2021@users.noreply.github.com>
2021-10-12 00:23:10 +0530

    Create three numbers.py
    the program created takes three different numbers as inputs and calculates the sum , product and the individual square of the numbers.

---
+++ three numbers.py
@@ -0,0 +1,11 @@
+n1=int(input("enter the first number of your choice-"))
+n2=int(input("enter the second number of your choice-"))
+n3=int(input("enter the third number of your choice-"))
+sum=n1+n2+n3
+product=n1*n2*n3
+sq1=n1**2
+sq2=n2**2
+sq3=n3**2
+print("the sum of the three is",sum)
+print("the product of three numbers is",product)
+print("the square of the three numbers is ",sq1,",",sq2,",",sq3,"respecrively")
