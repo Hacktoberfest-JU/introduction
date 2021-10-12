1529a59ecf88c6b429e0b3b6bd5ea08b902f8cc2
parikshit0108 <92357216+parikshit0108@users.noreply.github.com>
2021-10-12 12:39:25 +0530

    Create parikshit_21bcon299_4.py
    This program calculates HCF of two numbers

---
+++ parikshit_21bcon299_4.py
@@ -0,0 +1,14 @@
+def compute_hcf(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
+
+num1 = int(input())
+num2 = int(input())
+
+print("The H.C.F. is", compute_hcf(num1, num2))
