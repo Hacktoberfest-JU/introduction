240766fadced8de5fac810e0c8194a756ac58c75
kirtagya21bcon047 <91947719+kirtagya21bcon047@users.noreply.github.com>
2021-10-12 15:02:31 +0530

    kirtagya21bcon047*3.cpp

---
+++ Kirtagya21bcon047*3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //per packet of chocolate is costing 10 rupees
+  // a is the number of the chocolates
+  int a,b ;
+  cout<<"enter the value of chocolates you want"<<endl;
+  cin>>a;
+  //total price of the chocolate is 10*a or b=10*a
+  b=10*a;
+  cout<<b<<endl;
+  return 0;
+}
+  
+  
