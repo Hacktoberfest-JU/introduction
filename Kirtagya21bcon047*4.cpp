32268baf16bb3ad0946158429d1e362ae8cac712
kirtagya21bcon047 <91947719+kirtagya21bcon047@users.noreply.github.com>
2021-10-13 18:50:07 +0530

    Create Kirtagya21bcon047*4.cpp
    I hope you like it.

---
+++ Kirtagya21bcon047*4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int i,Girish,Manoj,Jitendra;
+    cout<<"enter the name of employee"<<endl;
+    
+    if(i=Girish){
+        cout<<"Name-Girish"<<endl<<"Eployee code-1090"<<endl;  
+          }
+          if(i=Manoj){
+              cout<<"Name-Manoj"<<endl<<"Employee code-2200"<<endl;
+          }
+          if(i=Jitendra){
+              cout<<"Name-Jitendra"<<endl<<"Employee code-2021"<<endl;
+          }
+
+    return 0;
+}
