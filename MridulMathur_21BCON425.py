b48e60af62870e6bb42a852f25daff5de6a14d96
mridul-mathur <91665131+mridul-mathur@users.noreply.github.com>
2021-10-13 16:22:25 +0530

    Create MridulMathur_21BCON425.py

---
+++ MridulMathur_21BCON425.py
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    int p,n;
+    float r,si;
+    printf("enter the value of p,n,r \n");
+    scanf("%d %d %f" ,&p,&n,&r);
+    si= p*n*r/100;
+    printf("simple intrest is equal to %f",si);
+    }
+    
