c8020a3336b5a9b083ef410458a225ef91c02bae
Divyansh Pareek <92642324+Div402@users.noreply.github.com>
2021-10-16 22:24:24 +0530

    Create Divyansh_21BON144.cpp

---
+++ Divyansh_21BON144.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=3;
+    int b=5;
+    int c=3;
+    int d;
+    d=a+b+c;
+    cout<<"Addition of numbers is:"<<d;
+    return 0;
+}
