944944b2aaeb6918d6723a4069488dc39bbe2f8b
HemangJoshi <91689621+HemangJoshi@users.noreply.github.com>
2021-10-18 14:33:59 +0530

    hemang joshi
    hemang joshi 21BCON330

---
+++ 21 bcon330.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+int main()
+{
+  int i,n,f=0;
+  std::cout<<"Enter a number ";
+  std::cin>>n;
+  for(i=n;i>=1;i--)
+  {
+  f=f+i;}
+  std::cout<<"the sum of all the number untill n is "<<f;
+return 0;
+
+}
