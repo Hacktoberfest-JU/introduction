e80b3178b2aef420471ba3a4a347835b41fbe476
RashiKhanna10 <92789792+RashiKhanna10@users.noreply.github.com>
2021-10-19 16:05:57 +0530

    Rashi_Khanna_21BCON233_04.py
    This code checks wether you can vote or not on the basis of your age

---
+++ Rashi_Khanna_21BCON233_04.py
@@ -0,0 +1,9 @@
+# MY NAME IS RASHI KHANNA
+# THIS PRAGAM TELLS YOU IF YOU CAN VOTE OR NOT, ON THE BASIS OF AGE
+
+
+age = int(input("Enter your age : "))
+if age >= 18:
+    print("You are eligible to vote :)")
+else :
+    print("You are not eligible to vote :(")
