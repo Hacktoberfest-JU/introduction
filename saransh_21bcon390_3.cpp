cbde5a0542fbc17427a319fd2e012a2d8e1e04c9
Saransh1650 <91770163+Saransh1650@users.noreply.github.com>
2021-10-20 00:22:22 +0530

    Create saransh_21bcon390_3.cpp

---
+++ saransh_21bcon390_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+//Here is the main program
+
+{
+    float pi=3.14;      //value of pi
+    float radius;            //value of radius
+
+    cout<<"Enter the radius of circle:\n";   //Takes input from user
+    cin>>radius;      // store value of radius
+    
+    cout<<"Area of circle is:\n"<<pi*radius*radius;
+
+   //Prints Area of circle
+
+    return 0;
+}
