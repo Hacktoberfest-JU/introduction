276e350b4b8c3c9767a03f1528f7754073ec1e75
RashiKhanna10 <92789792+RashiKhanna10@users.noreply.github.com>
2021-10-19 15:53:06 +0530

    Rashi_Khanna_21BCON233_02.py
    This file adds two numbers given by the user

---
+++ Rashi_Khanna_21BCON233_02.py
@@ -0,0 +1,7 @@
+# MY NAME IS RASHI KHANNA
+# THIS PROGRAM ADDS TWO NUMBERS GIVEN BY THE USER
+
+a = int(input("Please enter the first number : "))
+b = int(input("Please enter the first number : "))
+c = a + b
+print("The sum is :", c)
