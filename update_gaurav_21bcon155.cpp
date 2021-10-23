474fdcd5d1d828cf1a1a71468cadece45a2789bd
Gaurav Sharma <91673109+Gaurav058@users.noreply.github.com>
2021-10-23 20:34:35 +0530

    Create update_gaurav_21bcon155.cpp

---
+++ update_gaurav_21bcon155.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  cout<<"The lowercase alphabets are";
+  for (char la='a'; la<='z'; la++)
+  {
+    cout<<la<<" ";
+  }
+  getch();
+}
