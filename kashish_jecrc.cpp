396bc90e25e2014026610a1a3405b3439b90fc91
CodeKashish <91665510+CodeKashish@users.noreply.github.com>
2021-10-30 23:09:03 +0530

    Create kashish_jecrc.cpp

---
+++ kashish_jecrc.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int p,r,t,s;
+  cout<<"Enter the value of principel, rate, time";
+  cin>>p>>r>>t;
+  s=(p*r*t)/100;
+  cout<<"The simple intrest is "<<s;
+  getch();
+}
